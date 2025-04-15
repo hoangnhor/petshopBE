f1a50a5a58b3b64e3e889c4d7b7c532cb0bc3c5d
hoangnhor <hoangtranvan999@gmail.com>
2025-04-16 01:07:56 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,8 @@
-const express = require("express");
-const dotenv = require('dotenv');
-const mongoose = require("mongoose");
-const routes = require("./routes/index.js");
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
+const UserRouter = require('./UserRouter')
+const ProductRouter = require('./ProductRouter')
 
-dotenv.config();  // Load biến môi trường từ file .env
-
-const app = express();
-const port = process.env.PORT || 3030;
-
-app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-app.use(cookieParser());
-
-// Đăng ký các route API
-routes(app);
-
-// Kết nối đến MongoDB
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => {
-        console.log('Kết nối đến MongoDB thành công!');
-    })
-    .catch((err) => {
-        console.error('Lỗi khi kết nối MongoDB:', err.message);
-    });
-
-// Khởi động server backend
-app.listen(port, () => {
-    console.log('Server đang chạy trên cổng:', port);
-});
+const routes = (app) => {
+    app.use('/api/user', UserRouter)
+    app.use('/api/product', ProductRouter)
+}
+module.exports = routes
