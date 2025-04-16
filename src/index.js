f312059e36420e6649a01ac8890b0cbace93893d
hoangnhor <hoangtranvan999@gmail.com>
2025-04-17 04:20:19 +0700

    fix lo hong

--- src/index.js
+++ src/index.js
@@ -21,6 +21,12 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 
+// Thêm route cho /
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Welcome to PetshopBE API" });
+});
+
+// Route health check
 app.get("/healthz", (req, res) => {
     res.status(200).send("OK");
 });
