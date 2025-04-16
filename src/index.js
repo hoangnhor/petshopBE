0d00b3db8c1c23fc341d8230ea2a71eb12a39688
hoangnhor <hoangtranvan999@gmail.com>
2025-04-16 15:40:34 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,13 @@ dotenv.config();  // Load biến môi trường từ file .env
 const app = express();
 const port = process.env.PORT || 3030;
 
-app.use(cors());
+app.use(
+    cors({
+        origin: "https://petshop-fe.vercel.app", // Cho phép frontend gọi
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true
+    })
+);
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
@@ -32,4 +38,3 @@ mongoose.connect(process.env.MONGODB_URL, {
 app.listen(port, () => {
     console.log('Server đang chạy trên cổng:', port);
 });
-
